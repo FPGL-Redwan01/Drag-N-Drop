1b59d585a93ad966307254fecc286e2c022b2b5b
pranta-protik <pranta.cseku@gmail.com>
2021-03-27 19:39:48 +0600

    gamplay update

--- Assets/Scripts/DragFan.cs
+++ Assets/Scripts/DragFan.cs
@@ -6,12 +6,46 @@ using UnityEngine;
 public class DragFan : MonoBehaviour
 {
     [SerializeField] private float checkRadius;
+    [SerializeField] private float offset;
     private Vector3 _mouseOffset;
     private Vector3 _initialPosition;
+    private bool _isPlacedCorrectly;
+    private Transform _windFlowIndicatorPos;
+    private Transform _windFlowIndicatorNeg;
+    private Fan _fan;
 
     private void Awake()
     {
         _initialPosition = transform.position;
+        _windFlowIndicatorPos = transform.Find("windflowPos");
+        _windFlowIndicatorNeg = transform.Find("windflowNeg");
+        
+        _windFlowIndicatorPos.gameObject.SetActive(false);
+        _windFlowIndicatorNeg.gameObject.SetActive(false);
+    }
+
+    private void Start()
+    {
+        _fan = GetComponent<Fan>();
+    }
+
+    private void Update()
+    {
+        if (_isPlacedCorrectly)
+        {
+            _windFlowIndicatorPos.gameObject.SetActive(true);
+            _windFlowIndicatorNeg.gameObject.SetActive(true);
+            _windFlowIndicatorPos.transform.localEulerAngles = new Vector3(0, 0, _fan.forceAngle);
+            _windFlowIndicatorNeg.transform.localEulerAngles = new Vector3(0, 0, -1 * _fan.forceAngle);
+            Vector3 direction = UtilsClass.GetMouseWorldPosition() - transform.position;
+            // Debug.DrawRay(transform.position, direction);
+            transform.localEulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVector(direction) + offset);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Destroy(this);
+            }    
+        }
     }
 
     private void OnMouseDown()
@@ -26,25 +60,24 @@ public class DragFan : MonoBehaviour
 
     private void OnMouseUp()
     {
-        bool isCorrectlyPlaced = false;
-        
         Collider2D[] collider2DArray = Physics2D.OverlapCircleAll(transform.position, checkRadius);
         foreach (Collider2D collider2D in collider2DArray)
         {
             if (collider2D.CompareTag("Spike"))
             {
-                isCorrectlyPlaced = false;
+                _isPlacedCorrectly = false;
                 break;
             }
             if (collider2D.CompareTag("Border"))
             {
-                isCorrectlyPlaced = true;
+                _isPlacedCorrectly = true;
             }
         }
 
-        if (!isCorrectlyPlaced)
+        if (!_isPlacedCorrectly)
         {
             transform.position = _initialPosition;
+            return;
         }
     }
 
--- Assets/Scripts/Fan.cs
+++ Assets/Scripts/Fan.cs
@@ -7,25 +7,13 @@ using UnityEngine.UI;
 public class Fan : MonoBehaviour
 {
     [SerializeField] private float forceRadius;
-    [SerializeField] private float forceAngle;
-    [SerializeField] private Slider slider;
-
-    private void Awake()
-    {
-    }
+    public float forceAngle;
 
     private void Update()
     {
-        RotateFan();
         PushBalloon();
     }
-
-    private void RotateFan()
-    {
-        float rotationAngle = -1 * slider.value * forceAngle;
-        transform.localEulerAngles = new Vector3(0,0,rotationAngle);
-    }
-
+    
     private void PushBalloon()
     {
         Collider2D[] collider2DArray = Physics2D.OverlapCircleAll(transform.position, forceRadius);
@@ -36,7 +24,7 @@ public class Fan : MonoBehaviour
                 Balloon balloon = collider2D.GetComponent<Balloon>();
                 Vector2 forceDirection = balloon.transform.position - transform.position;
                 Debug.DrawRay(transform.position, forceDirection);
-                float angle = Vector2.Angle(forceDirection, transform.right);
+                float angle = Vector2.Angle(forceDirection, transform.up);
                 if (angle <= forceAngle)
                 {
                     balloon.AddForceToDirection(forceDirection,50f);
