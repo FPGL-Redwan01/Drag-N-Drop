e67a151311c5ec4dd30d63dedf2f0eeb7a2ecdb8
FPGL-Redwan01 <abir.cse.ku@gmail.com>
2021-03-28 00:33:47 +0600

    unnecessary files deleted

--- Assets/Scripts/InstructionTypeHolder.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class InstructionTypeHolder : MonoBehaviour
-{
-    public InstructionTypeSO instructionType;
-}
