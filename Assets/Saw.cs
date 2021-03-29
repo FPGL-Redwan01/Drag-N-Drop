cb13a421f9f79912c6199e86dfb64a92164476e8
FPGL-Redwan01 <abir.cse.ku@gmail.com>
2021-03-29 11:31:32 +0600

    level3

--- Assets/Saw.cs
+++ Assets/Saw.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class Saw : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public float delta = 1.5f;  // Amount to move left and right from the start point
+    public float speed = 2.0f;
+    private Vector3 startPos;
+    [SerializeField]
+    bool enableMovement;
+
     void Start()
     {
-        
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, 0, -1 * 10);
+
+        if (enableMovement)
+        {
+            Vector3 v = startPos;
+            v.x += delta * Mathf.Sin(Time.time * speed);
+            transform.position = v;
+        }
     }
 }
