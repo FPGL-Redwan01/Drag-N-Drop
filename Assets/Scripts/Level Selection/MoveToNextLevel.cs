b0eb65ca6294a4c3e50dacace0fa7e57f9ef39f7
FPGL-Redwan01 <abir.cse.ku@gmail.com>
2021-03-29 17:05:21 +0600

    level unlock system

---
+++ Assets/Scripts/Level Selection/MoveToNextLevel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveToNextLevel : MonoBehaviour
+{
+    public int nextSceneLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextSceneLoad = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(MoveToNext());
+        }
+    }
+    IEnumerator MoveToNext()
+    {
+        yield return new WaitForSeconds(2f);
+        if (SceneManager.GetActiveScene().buildIndex == 4)
+        {
+            SceneManager.LoadScene(1);
+
+
+        }
+        else
+        {
+
+            SceneManager.LoadScene(nextSceneLoad);
+
+
+            if (nextSceneLoad > PlayerPrefs.GetInt("levelAt"))
+            {
+                PlayerPrefs.SetInt("levelAt", nextSceneLoad);
+            }
+        }
+    }
+
+}
