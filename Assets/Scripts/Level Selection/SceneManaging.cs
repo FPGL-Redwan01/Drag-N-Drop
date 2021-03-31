03006ba06d4d11ee66961e71e422137432488270
FPGL-Redwan01 <abir.cse.ku@gmail.com>
2021-03-31 18:56:15 +0600

    scene playrprefs added

--- Assets/Scripts/Level Selection/SceneManaging.cs
+++ Assets/Scripts/Level Selection/SceneManaging.cs
@@ -8,11 +8,36 @@ public class SceneManaging : MonoBehaviour
 
     public void ChangeScene(int sceneIndex)
     {
-        SceneManager.LoadScene(sceneIndex);
+
+            SceneManager.LoadScene(sceneIndex);
     }
 
     public void QuitGame()
     {
         Application.Quit();
     }
+
+    public void ChangeSceneForStart(int sceneIndex)
+    {
+        int a;
+        a = PlayerPrefs.GetInt("TutorialPlayed");
+        if (a == 1)
+        {
+            SceneManager.LoadScene(3);
+        }
+        else
+        SceneManager.LoadScene(sceneIndex);
+
+    }
+    public void ChangeSceneForTutorial(int sceneIndex)
+    {
+        if (PlayerPrefs.GetInt("TutorialPlayed", 0) <= 0)
+        {
+
+            PlayerPrefs.SetInt("TutorialPlayed", 1);
+        }
+            SceneManager.LoadScene(sceneIndex);
+
+    }
+
 }
